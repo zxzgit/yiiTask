a3cfcbb3b25fcafd19eea71364aa7622b81a50e0
zxzgit <578226426@qq.com>
2016-12-27 18:34:06 +0800

    api study3

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -8,18 +8,46 @@
 namespace app\controllers;
 
 use yii\rest\ActiveController;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBasicAuth;
+use yii\filters\auth\HttpBearerAuth;
+use yii\filters\auth\QueryParamAuth;
 
 class UserController extends ActiveController
 {
 	public $modelClass = 'app\models\common\User';
+	
+	public function init() {
+		parent::init();
+		\Yii::$app->user->enableSession = false;
+		\Yii::$app->response->formatters=[
+			\yii\web\Response::FORMAT_JSON => [
+				'class' => 'yii\web\JsonResponseFormatter',
+				'prettyPrint' => YII_DEBUG, // use "pretty" output in debug mode
+				'encodeOptions' => JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE,
+				// ...
+			]
+		];
+	}
+	
 	public function checkAccess($action, $model = null, $params = [])
 	{
 		//todo something
 //		echo $action;
 	}
 	
-	public function actionView(){
-		echo "hello world";
+	public function behaviors()
+	{
+		$behaviors = parent::behaviors();
+		$behaviors['authenticator'] = [
+			'class' => CompositeAuth::className(),
+			'authMethods' => [
+				HttpBasicAuth::className(),
+				HttpBearerAuth::className(),
+				QueryParamAuth::className(),
+			],
+		];
+		return $behaviors;
 	}
 
 }
